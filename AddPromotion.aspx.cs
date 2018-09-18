ba082788c1226bae6ea396f27b4216c83eca2a9a
AnubhaGujral <30989906+AnubhaGujral@users.noreply.github.com>
2018-09-19 09:49:59 +1000

    Add Promotion, View Admin and View Branch

---
+++ AddPromotion.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Grocery_Demo
+{
+    public partial class AddPromotion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UsernameAdmin"] != null && Session["PasswordAdmin"] != null && Session["BranchAdmin"] != null)
+            {
+                string username = Session["UsernameAdmin"].ToString();
+                string password = Session["PasswordAdmin"].ToString();
+                string branch = Session["BranchAdmin"].ToString();
+                string CS;
+                CS = "Data Source=DESKTOP-UEA1TMT;Initial Catalog=Grocery_Demo;Integrated Security=True";
+                SqlConnection con = new SqlConnection(CS);
+                SqlCommand cmd = new SqlCommand("AdminValidation", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@AdministratorUsername", username);
+                cmd.Parameters.AddWithValue("@AdministratorPassword", password);
+                cmd.Parameters.AddWithValue("@GroceryBranchName", branch);
+                con.Open();
+                SqlDataReader read = cmd.ExecuteReader();
+                read.Read();
+                if (read.HasRows == false)
+                {
+                    Response.Redirect("SignIn.aspx");
+                }
+                con.Close();
+            }
+
+            else
+            {
+                Response.Redirect("SignIn.aspx");
+            }
+
+            //Label1.Text = Session["BranchAdmin"].ToString();
+            //Label2.Text = Session["UsernameAdmin"].ToString();
+        }
+    }
+}
